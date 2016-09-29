df0e8b646f768ac58a32c9591b2581416b7e0eae
BirdLin <BirdLin@NB-HP850-D2R.newstl.com.tw>
2016-09-29 16:07:42 +0800

    Add selenium / open by chrome & phantomjs function

--- test2.py
+++ test2.py
@@ -2,46 +2,30 @@
 '''
 @author: birdlin
 '''
-import re
-import json
-from urllib.request import urlopen
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-from openpyxl import Workbook
-from builtins import int
 
-#wb = Workbook()
-#ws = wb.active
+#driver = webdriver.Chrome(r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
+driver = webdriver.PhantomJS(r'C:\Users\birdlin\AppData\Local\Programs\Python\Python35-32\phantomjs-2.1.1-windows\bin\phantomjs')
+driver.get("http://financials.morningstar.com/ratios/r.html?t=UA&region=USA&culture=en_US")
+#driver.get("http://www.python.org")
+assert "Morningstar" in driver.title
+soup = BeautifulSoup(driver.page_source, 'lxml')
+for title in soup.find_all('td'):
+    print (title)
+
+driver.quit()
+
+
+''' 
+driver = webdriver.Chrome(r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
+driver.get("http://www.google.com.tw")
+#driver.get("http://www.python.org")
+assert "Google" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("python")
+elem.send_keys(Keys.RETURN)
+print (driver.page_source)
 '''
-url = 'http://financials.morningstar.com/ratios/r.html?t=UA&region=usa&culture=en-US'
-#list = []
-with urlopen(url) as response:
-    soup = BeautifulSoup(response.read(), 'lxml')
-
-#for th in (soup.find_all('thead')):
-print (soup.find_all('script'))
-'''
-url = 'http://dcsd.nutrislice.com/menu/meadow-view/lunch/'
-
-with urlopen(url) as text:
-    p = re.compile(r'bootstrapData', text)
-    menu = json.loads(p.string)
-    print (menu)
-
-'''
-text = urlopen('http://dcsd.nutrislice.com/menu/meadow-view/lunch/').read()
-menu = json.loads(re.search(r"bootstrapData\['menuMonthWeeks'\]\s*=\s*(.*);", text).group(1))
-'''
-#print (text)
-
-
-'''
-for tag in (soup.select("tbody:nth-of-type(1)")):
-    for number in tag.find_all('td', class_ = re.compile(r'12')) :
-        for string in number.stripped_strings :
-            ws.cell(row=row_index, column=column_index).value=string
-            column_index += 1
-            if column_index > 15 :
-                row_index += 1
-                column_index = 1
-'''
-
