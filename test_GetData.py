6e6a4bf1a087d2e2eb9916b4df9d0661dd235368
bigjyh1979 <bigjyh1979@gmail.com>
2016-10-12 23:14:06 +0800

    Update test_GetData.py

--- test_GetData.py
+++ test_GetData.py
@@ -7,21 +7,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
-
-driver = webdriver.Chrome(r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
-driver.get("http://portfolio.morningstar.com/Rtport/Reg/MyView.aspx?ViewPage=5#44-shownewsByDate")
-assert "Morningstar" in driver.title
-
-#driver = webdriver.PhantomJS(r'C:\Users\birdlin\AppData\Local\Programs\Python\Python35-32\phantomjs-2.1.1-windows\bin\phantomjs')
-#driver.get("http://financials.morningstar.com/ratios/r.html?t=UA&region=USA&culture=en_US")
-#assert "Morningstar" in driver.title
-
-#driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-#driver.switch_to_window(driver.window_handles[1])
+#driver = webdriver.Chrome(r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
+driver = webdriver.PhantomJS(r'C:\Users\birdlin\AppData\Local\Programs\Python\Python35-32\phantomjs-2.1.1-windows\bin\phantomjs')
+driver.get("http://financials.morningstar.com/ratios/r.html?t=UA&region=USA&culture=en_US")
 #driver.get("http://www.python.org")
-
+assert "Morningstar" in driver.title
 soup = BeautifulSoup(driver.page_source, 'lxml')
 for title in soup.find_all('td'):
     for string in title.stripped_strings :
         print (string)
 
+driver.quit()
+
+
+''' 
+driver = webdriver.Chrome(r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
+driver.get("http://www.google.com.tw")
+#driver.get("http://www.python.org")
+assert "Google" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("python")
+elem.send_keys(Keys.RETURN)
+print (driver.page_source)
+'''
